0dfc63ae2bde0e99bdc186f778a504ee517b81e8
Darkweak <sylvain@les-tilleuls.coop>
2021-02-20 00:32:02 +0100

    Use channel to handle keysaver update

--- cache/keysaver/keySaver.go
+++ cache/keysaver/keySaver.go
@@ -2,21 +2,55 @@ package keysaver
 
 import (
 	"github.com/dgraph-io/ristretto/z"
+	"reflect"
 	"sync"
+	"time"
 )
+
+const ADD = "add"
+const DELETE = "delete"
+
+type payload struct {
+	updateType string
+	clearKey   string
+	hashedKey  uint64
+}
 
 // ClearKey contains an array referencing the keys saved in the cache provider
 // and implements Mutex to ensure the array didn't get updated during
 // inspection or update. It ensure deadlocks or any crash won't happen.
 type ClearKey struct {
-	keys map[uint64]string
-	mu sync.RWMutex
+	keys           map[uint64]string
+	mu             sync.RWMutex
+	payloadChannel chan payload
+}
+
+func (c *ClearKey) startKeysUpdater() {
+	for {
+		payload, _ := <-c.payloadChannel
+		if reflect.ValueOf(&c.mu).Elem().FieldByName("readerCount").Int() > 0 {
+			time.Sleep(20 * time.Millisecond)
+			c.payloadChannel <- payload
+		}
+		c.mu.RLock()
+		switch payload.updateType {
+		case ADD:
+			c.keys[payload.hashedKey] = payload.clearKey
+			break
+		case DELETE:
+			delete(c.keys, payload.hashedKey)
+			break
+		}
+		c.mu.RUnlock()
+	}
 }
 
 // NewClearKey generate a new ClearKey object and returns it
 func NewClearKey() *ClearKey {
 	ck := &ClearKey{}
 	ck.keys = make(map[uint64]string)
+	ck.payloadChannel = make(chan payload)
+	go ck.startKeysUpdater()
 	return ck
 }
 
@@ -35,18 +69,22 @@ func (c *ClearKey) ListKeys() []string {
 // AddKey is called to update keys array with non-existing key or replacing
 // the existing one.
 func (c *ClearKey) AddKey(clearKey string) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
 	hashedKey, _ := z.KeyToHash(clearKey)
-	c.keys[hashedKey] = clearKey
+	c.payloadChannel <- payload{
+		ADD,
+		clearKey,
+		hashedKey,
+	}
 }
 
 // DelKey is called to delete a key if exists, it does nothing otherwise
 func (c *ClearKey) DelKey(clearKey string, hashedKey uint64) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
 	if clearKey != "" {
 		hashedKey, _ = z.KeyToHash(clearKey)
 	}
-	delete(c.keys, hashedKey)
+	c.payloadChannel <- payload{
+		DELETE,
+		clearKey,
+		hashedKey,
+	}
 }
