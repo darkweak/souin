ab38615f372746a023db58715f9f9cf778b21704
Darkweak <sylvain@les-tilleuls.coop>
2021-07-09 02:03:40 +0200

    feat: Replace VaryLayerStorage to plain storage system with algorithm (#93)
    * feat: Replace VaryLayerStorage to plain storage system with algorithm
    
    * Remove caddy update
    
    * Fix coalescing + vary mannagement
    
    * Use plain concatenation instead of Sprintf method for varied key

--- cache/types/layerStorage.go
+++ cache/types/layerStorage.go
@@ -4,39 +4,6 @@ import (
 	"github.com/dgraph-io/ristretto"
 )
 
-// VaryLayerStorage is the layer for Vary support storage
-type VaryLayerStorage struct {
-	*ristretto.Cache
-}
-
-// InitializeVaryLayerStorage initialize the storage
-func InitializeVaryLayerStorage() *VaryLayerStorage {
-	storage, _ := ristretto.NewCache(&ristretto.Config{
-		NumCounters: 1e7,     // number of keys to track frequency of (10M).
-		MaxCost:     1 << 30, // maximum cost of cache (1GB).
-		BufferItems: 64,      // number of keys per Get buffer.
-	})
-
-	return &VaryLayerStorage{Cache: storage}
-}
-
-// Get method returns the varied headers list if exists, empty array then
-func (provider *VaryLayerStorage) Get(key string) []string {
-	val, found := provider.Cache.Get(key)
-	if !found {
-		return []string{}
-	}
-	return val.([]string)
-}
-
-// Set method will store the response in Ristretto provider
-func (provider *VaryLayerStorage) Set(key string, headers []string) {
-	isSet := provider.Cache.Set(key, headers, 1)
-	if !isSet {
-		panic("Impossible to set value into Ristretto")
-	}
-}
-
 // CoalescingLayerStorage is the layer to be able to not coalesce uncoalesceable request
 type CoalescingLayerStorage struct {
 	*ristretto.Cache
--- cache/types/providers.go
+++ cache/types/providers.go
@@ -2,12 +2,14 @@ package types
 
 import (
 	"github.com/darkweak/souin/configurationtypes"
+	"net/http"
 	"time"
 )
 
 // AbstractProviderInterface should be implemented in any providers
 type AbstractProviderInterface interface {
 	ListKeys() []string
+	Prefix(key string, req *http.Request) []byte
 	Get(key string) []byte
 	Set(key string, value []byte, url configurationtypes.URL, duration time.Duration)
 	Delete(key string)
