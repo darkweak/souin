691b0cfeb3056e07dce1b8fb8f1b4f96ca87b173
Darkweak <sylvain@les-tilleuls.coop>
2021-09-30 02:01:28 +0200

    Update trafik vendor

--- plugins/traefik/vendor/github.com/darkweak/souin/configurationtypes/types.go
+++ plugins/traefik/vendor/github.com/darkweak/souin/configurationtypes/types.go
@@ -71,6 +71,7 @@ type CacheProvider struct {
 // CDN config
 type CDN struct {
 	APIKey   string `json:"api_key,omitempty" yaml:"api_key,omitempty"`
+	Dynamic  string `json:"dynamic,omitempty" yaml:"dynamic,omitempty"`
 	Hostname string `json:"hostname,omitempty" yaml:"hostname,omitempty"`
 	Network  string `json:"network,omitempty" yaml:"network,omitempty"`
 	Provider string `json:"provider,omitempty" yaml:"provider,omitempty"`
