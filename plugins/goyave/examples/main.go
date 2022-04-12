0fc14c901de52ab3af4f91d18698648919e3a9d1
darkweak <darkweak@protonmail.com>
2022-04-12 23:25:22 +0200

    Fix config.json not found

--- plugins/goyave/examples/main.go
+++ plugins/goyave/examples/main.go
@@ -5,9 +5,11 @@ import (
 
 	cache "github.com/darkweak/souin/plugins/goyave"
 	"goyave.dev/goyave/v4"
+	"goyave.dev/goyave/v4/config"
 )
 
 func main() {
+	config.LoadFrom("examples/config.json")
 	goyave.Start(func(r *goyave.Router) {
 		r.Get("/{p}", func(response *goyave.Response, r *goyave.Request) {
 			response.String(http.StatusOK, "Hello, World 👋!")
