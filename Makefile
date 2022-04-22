5f16dfbbba8d28b7479a91b7e86a7391787aa385
darkweak <darkweak@protonmail.com>
2022-04-23 01:23:10 +0200

    feat(plugins): Enhance caddy documentation (#206)
    * feat(plugins): Enhance caddy documentation
    
    * feat(plugins): Introduce dotweb plugin (#207)

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-.PHONY: build-and-run-caddy build-and-run-caddy-json build-and-run-chi build-and-run-echo build-and-run-fiber build-and-run-gin \
-	build-and-run-goyave build-and-run-skipper build-and-run-souin build-and-run-traefik build-and-run-tyk build-and-run-webgo \
-	build-app build-caddy build-dev coverage create-network down env-dev env-prod gatling generate-plantUML golangci-lint \
-	health-check-prod help lint log tests up validate vendor-plugins
+.PHONY: build-and-run-caddy build-and-run-caddy-json build-and-run-chi build-and-run-dotweb build-and-run-echo build-and-run-fiber \
+	build-and-run-gin build-and-run-goyave build-and-run-skipper build-and-run-souin build-and-run-traefik build-and-run-tyk \
+	build-and-run-webgo build-app build-caddy build-dev coverage create-network down env-dev env-prod gatling generate-plantUML \
+	golangci-lint health-check-prod help lint log tests up validate vendor-plugins
 
 DC=docker-compose
 DC_BUILD=$(DC) build
@@ -17,6 +17,9 @@ build-and-run-caddy-json:  ## Run caddy binary with the json configuration
 
 build-and-run-chi:  ## Run Chi with Souin as plugin
 	cd plugins/chi && $(MAKE) prepare
+
+build-and-run-dotweb:  ## Run Dotweb with Souin as plugin
+	cd plugins/dotweb && $(MAKE) prepare
 
 build-and-run-echo:  ## Run Echo with Souin as plugin
 	cd plugins/echo && $(MAKE) prepare
@@ -108,6 +111,7 @@ validate: lint tests down health-check-prod ## Run lint, tests and ensure prod c
 
 vendor-plugins: ## Generate and prepare vendors for each plugin
 	cd plugins/chi && $(MAKE) vendor
+	cd plugins/dotweb && $(MAKE) vendor
 	cd plugins/echo && $(MAKE) vendor
 	cd plugins/fiber && $(MAKE) vendor
 	cd plugins/gin && $(MAKE) vendor
---
+++ plugins/dotweb/souin.go
@@ -0,0 +1,128 @@
+package dotweb
+
+import (
+	"bytes"
+	"context"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/darkweak/souin/api"
+	"github.com/darkweak/souin/cache/coalescing"
+	"github.com/darkweak/souin/configurationtypes"
+	"github.com/darkweak/souin/plugins"
+	"github.com/darkweak/souin/rfc"
+	"github.com/devfeel/dotweb"
+)
+
+const (
+	getterContextCtxKey key = "getter_context"
+)
+
+var (
+	DefaultConfiguration = Configuration{
+		DefaultCache: &configurationtypes.DefaultCache{
+			TTL: configurationtypes.Duration{
+				Duration: 10 * time.Second,
+			},
+		},
+		LogLevel: "info",
+	}
+	DevDefaultConfiguration = Configuration{
+		API: configurationtypes.API{
+			BasePath: "/souin-api",
+			Prometheus: configurationtypes.APIEndpoint{
+				Enable: true,
+			},
+			Souin: configurationtypes.APIEndpoint{
+				Enable: true,
+			},
+		},
+		DefaultCache: &configurationtypes.DefaultCache{
+			Regex: configurationtypes.Regex{
+				Exclude: "/excluded",
+			},
+			TTL: configurationtypes.Duration{
+				Duration: 5 * time.Second,
+			},
+		},
+		LogLevel: "debug",
+	}
+)
+
+// SouinDotwebMiddleware declaration.
+type (
+	key                   string
+	SouinDotwebMiddleware struct {
+		dotweb.BaseMiddleware
+		plugins.SouinBasePlugin
+		Configuration *Configuration
+		bufPool       *sync.Pool
+	}
+	getterContext struct {
+		next func(ctx dotweb.Context) error
+		rw   http.ResponseWriter
+		req  *http.Request
+	}
+)
+
+func NewHTTPCache(c Configuration) *SouinDotwebMiddleware {
+	s := SouinDotwebMiddleware{}
+	s.Configuration = &c
+	s.bufPool = &sync.Pool{
+		New: func() interface{} {
+			return new(bytes.Buffer)
+		},
+	}
+
+	s.Retriever = plugins.DefaultSouinPluginInitializerFromConfiguration(&c)
+	s.RequestCoalescing = coalescing.Initialize()
+	s.MapHandler = api.GenerateHandlerMap(s.Configuration, s.Retriever.GetTransport())
+
+	return &s
+}
+
+func (s *SouinDotwebMiddleware) Handle(c dotweb.Context) error {
+	req := s.Retriever.GetContext().Method.SetContext(c.Request().Request)
+	rw := c.Response().Writer()
+	if b, handler := s.HandleInternally(req); b {
+		handler(rw, req)
+
+		return nil
+	}
+
+	if !plugins.CanHandle(req, s.Retriever) {
+		rw.Header().Add("Cache-Status", "Souin; fwd=uri-miss")
+		return s.Next(c)
+	}
+
+	customWriter := &plugins.CustomWriter{
+		Response: &http.Response{},
+		Buf:      s.bufPool.Get().(*bytes.Buffer),
+		Rw:       rw,
+	}
+	req = s.Retriever.GetContext().SetContext(req)
+	getterCtx := getterContext{s.Next, customWriter, req}
+	ctx := context.WithValue(req.Context(), getterContextCtxKey, getterCtx)
+	req = req.WithContext(ctx)
+	if plugins.HasMutation(req, rw) {
+		return s.Next(c)
+	}
+	req.Header.Set("Date", time.Now().UTC().Format(time.RFC1123))
+	combo := ctx.Value(getterContextCtxKey).(getterContext)
+	c.Request().Request = req
+	c.Response().SetWriter(customWriter)
+
+	return plugins.DefaultSouinPluginCallback(customWriter, req, s.Retriever, nil, func(_ http.ResponseWriter, _ *http.Request) error {
+		var e error
+		combo.next(c)
+
+		combo.req.Response = customWriter.Response
+		if combo.req.Response, e = s.Retriever.GetTransport().(*rfc.VaryTransport).UpdateCacheEventually(combo.req); e != nil {
+			return e
+		}
+
+		_, _ = customWriter.Send()
+		return e
+	})
+}
