dcc27e73bd82f1a57be14994480e83e857af87bf
darkweak <darkweak@protonmail.com>
2022-07-10 00:51:16 +0200

    Increase Skipper wait duration

--- .github/workflows/workflow_plugins_generator.sh
+++ .github/workflows/workflow_plugins_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 plugins=("beego"  "chi"  "dotweb"  "echo"  "fiber"  "gin"  "go-zero"  "goyave"  "kratos"  "skipper"  "souin"  "traefik"  "tyk"  "webgo")
-durations=("35"   "30"   "30"      "30"    "45"     "30"   "50"       "35"      "50"      "50"       "40"     "30"       "30"   "30")
+durations=("35"   "30"   "30"      "30"    "45"     "30"   "70"       "35"      "50"      "60"       "40"     "30"       "30"   "30")
 versions=("16"    "16"   "16"      "16"    "16"     "16"   "16"       "16"      "18"      "18"       "16"     "16"       "16"   "16")
 
 IFS= read -r -d '' tpl <<EOF
