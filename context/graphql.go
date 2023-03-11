be7e6ef8a66c76ee8028384391a3d2bfce49007c
darkweak <darkweak@protonmail.com>
2023-03-11 16:02:35 +0100

    fix(chore): request body consumed

--- context/graphql.go
+++ context/graphql.go
@@ -41,6 +41,7 @@ func (g *graphQLContext) SetContext(req *http.Request) *http.Request {
 	if g.custom && req.Body != nil {
 		b := bytes.NewBuffer([]byte{})
 		_, _ = io.Copy(b, req.Body)
+		req.Body = io.NopCloser(b)
 		if b.Len() > 0 {
 			if isMutation(b.Bytes()) {
 				ctx = context.WithValue(ctx, IsMutationRequest, true)
