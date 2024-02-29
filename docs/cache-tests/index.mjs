bfcb800bf0b3a3ab20a83d3a4679614c450c1ddd
darkweak <darkweak@protonmail.com>
2024-02-29 21:17:04 +0100

    debug mr.json and test screenshot

--- docs/cache-tests/index.mjs
+++ docs/cache-tests/index.mjs
@@ -1,14 +1,14 @@
 export default [
   {
     file: 'caddy.json',
-    name: 'caddy',
+    name: 'Caddy',
     type: 'rev-proxy',
     version: '0.7.0',
     link: 'https://github.com/http-tests/cache-tests/wiki/Caddy'
   },
   {
     file: 'mr.json',
-    name: 'Souin',
+    name: 'Merge request',
     type: 'rev-proxy',
     version: '0.0.1',
     link: 'https://github.com/http-tests/cache-tests/wiki/Caddy'
