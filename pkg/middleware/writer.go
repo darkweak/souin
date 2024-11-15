5da5607b327d92528faeb19ebda933fa83996396
darkweak <darkweak@protonmail.com>
2024-11-15 19:17:50 +0100

    fix(writer): buffer race condition

--- pkg/middleware/writer.go
+++ pkg/middleware/writer.go
@@ -39,6 +39,12 @@ type CustomWriter struct {
 	statusCode  int
 }
 
+func (r *CustomWriter) handleBuffer(callback func(*bytes.Buffer)) {
+	r.mutex.Lock()
+	callback(r.Buf)
+	r.mutex.Unlock()
+}
+
 // Header will write the response headers
 func (r *CustomWriter) Header() http.Header {
 	r.mutex.Lock()
