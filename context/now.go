ed12ace71505cd585c11d00117927771efd5dc8e
darkweak <darkweak@protonmail.com>
2024-12-22 21:04:35 +0100

    fix(now context): https://github.com/caddyserver/cache-handler/issues/116

--- context/now.go
+++ context/now.go
@@ -19,8 +19,16 @@ func (*nowContext) SetContextWithBaseRequest(req *http.Request, _ *http.Request)
 func (cc *nowContext) SetupContext(_ configurationtypes.AbstractConfigurationInterface) {}
 
 func (cc *nowContext) SetContext(req *http.Request) *http.Request {
-	now := time.Now().UTC()
-	req.Header.Set("Date", now.Format(time.RFC1123))
+	var now time.Time
+	var e error
+
+	now, e = time.Parse(time.RFC1123, req.Header.Get("Date"))
+
+	if e != nil {
+		now := time.Now()
+		req.Header.Set("Date", now.Format(time.RFC1123))
+	}
+
 	return req.WithContext(context.WithValue(req.Context(), Now, now))
 }
 
